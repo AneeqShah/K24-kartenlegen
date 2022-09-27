5e8f114a14e196abb08b09bfcd828e889a450a52
aneeqshah <aneeqshah15@gmail.com>
2022-09-28 00:53:51 +0500

    order complete

--- lib/presentation/views/product_detail/layout/product_detail_body.dart
+++ lib/presentation/views/product_detail/layout/product_detail_body.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:k24/app/Custom_image_container.dart';
+import 'package:k24/app/custom_loader.dart';
 import 'package:k24/presentation/elements/custom_text.dart';
 import 'package:k24/presentation/views/product_detail/layout/widgets/select_payment.dart';
 
@@ -27,79 +31,155 @@ class ProductDetailBody extends StatefulWidget {
 }
 
 class _ProductDetailBodyState extends State<ProductDetailBody> {
+  String userID = FirebaseAuth.instance.currentUser!.uid;
+  TextEditingController _question = TextEditingController();
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            CustomImageContainer(
-                height: 200,
-                wight: MediaQuery.of(context).size.width,
-                radius: 8,
-                image: widget.image),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomText(
-                  text: widget.title,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
-                ),
-                CustomText(text: '€ ${widget.price}')
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomText(
-              text: 'Range',
-              fontSize: 13,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey.shade600,
-            ),
-            CustomText(
-              text: '${widget.minRange} to ${widget.maxRange} words',
-              fontWeight: FontWeight.w400,
-              fontSize: 13,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomText(
-              text: 'Description',
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
-            ),
-            CustomText(
-              text: widget.description,
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              height: 1.9,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(),
-            SizedBox(
-              height: 10,
-            ),
-            SelectPayment()
-          ],
+    return CustomLoader(
+      isLoading: isLoading,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              CustomImageContainer(
+                  height: 200,
+                  wight: MediaQuery.of(context).size.width,
+                  radius: 8,
+                  image: widget.image),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CustomText(
+                    text: widget.title,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  CustomText(text: '€ ${widget.price}')
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              CustomText(
+                text: 'Range',
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey.shade600,
+              ),
+              CustomText(
+                text: '${widget.minRange} to ${widget.maxRange} words',
+                fontWeight: FontWeight.w400,
+                fontSize: 13,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 10,
+              ),
+              CustomText(
+                text: 'Description',
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+              CustomText(
+                text: widget.description,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                height: 1.9,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 10,
+              ),
+              SelectPayment(
+                controller: _question,
+                onApple: () {
+                  _confirmOrder();
+                },
+                onPaypal: () {},
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  _confirmOrder() async {
+    try {
+      loadingTrue();
+      String orderID = FirebaseFirestore.instance.collection("order").doc().id;
+      await FirebaseFirestore.instance.collection("order").doc(orderID).set({
+        "orderID": orderID,
+        "customerID": userID,
+        "min": widget.minRange,
+        "max": widget.maxRange,
+        "title": widget.title,
+        "orderTime": DateTime.now().millisecondsSinceEpoch,
+        "isAnswered": false,
+        "price": widget.price,
+      }).then((value) async {
+        String chatID = FirebaseFirestore.instance
+            .collection("chat")
+            .doc(orderID)
+            .collection("messages")
+            .doc()
+            .id;
+        await FirebaseFirestore.instance
+            .collection("chat")
+            .doc(orderID)
+            .collection("messages")
+            .doc(chatID)
+            .set({
+          "userID": userID,
+          "question": _question.text,
+          "orderID": orderID,
+          "chatID": chatID,
+          "time": DateTime.now().millisecondsSinceEpoch,
+        }).then((value) async {
+          String graphID =
+              FirebaseFirestore.instance.collection("graph").doc().id;
+          await FirebaseFirestore.instance
+              .collection("graph")
+              .doc(graphID)
+              .set({
+            "price": widget.price,
+            "customerID": userID,
+            "graphID": graphID
+          }).then((value) {
+            loadingFalse();
+            print("Finished");
+          });
+        });
+      });
+    } on FirebaseException catch (e) {
+      loadingFalse();
+      Fluttertoast.showToast(msg: e.message!);
+    }
+  }
+
+  loadingTrue() {
+    isLoading = true;
+    setState(() {});
+  }
+
+  loadingFalse() {
+    isLoading = false;
+    setState(() {});
   }
 }
--- lib/presentation/views/product_detail/layout/widgets/select_payment.dart
+++ lib/presentation/views/product_detail/layout/widgets/select_payment.dart
@@ -2,31 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:k24/presentation/elements/app_button.dart';
 import 'package:k24/presentation/elements/custom_text.dart';
 
+import '../../../../elements/custom_textfield.dart';
+
 class SelectPayment extends StatelessWidget {
-  const SelectPayment({Key? key}) : super(key: key);
+  final TextEditingController controller;
+  final Function onApple;
+  final Function onPaypal;
+
+  const SelectPayment(
+      {super.key,
+      required this.controller,
+      required this.onApple,
+      required this.onPaypal});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CustomText(
-              text:
-              'Select payment'
-              'Method',
+              text: 'Ask your Question?',
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            AuthTextField(hint: 'Write your question here..', controller: controller),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomText(
+              text: 'Select payment'
+                  'Method',
               fontSize: 14,
               fontWeight: FontWeight.w600,
             ),
-            SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Row(
               children: [
-                Expanded(child: AppButton(onPressed: () {  }, text: 'Apple Pay',
+                Expanded(
+                    child: AppButton(
+                  onPressed: () => onApple(),
+                  text: 'Apple Pay',
                   fontSize: 11,
                 )),
-                SizedBox(width: 10,),
-                Expanded(child: AppButton(onPressed: () {  }, text: 'PayPal',
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                    child: AppButton(
+                  onPressed: () => onPaypal(),
+                  text: 'PayPal',
                   fontSize: 11,
                 )),
               ],
